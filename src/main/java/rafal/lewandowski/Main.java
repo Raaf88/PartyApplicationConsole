98f40b86a7b9f356dd43179ebed5499346ca6fc2
Raaf88 <rafalewandowski8@gmail.com>
2023-01-22 21:56:52 +0100

    Stworzenie menu aplikacji w pętli While

--- src/main/java/rafal/lewandowski/Main.java
+++ src/main/java/rafal/lewandowski/Main.java
@@ -1,7 +1,20 @@
 package rafal.lewandowski;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while(true){
+            System.out.println("Wybierz opcję: ");
+            System.out.println("1. Wyświetl gości");
+            System.out.println("2. Dodaj gościa");
+            System.out.println("3. Wyświetl potrawy");
+            System.out.println("4. Znajdź po numerze telefonu");
+            System.out.println("5. Wyjście");
+
+        }
 
     }
 }
