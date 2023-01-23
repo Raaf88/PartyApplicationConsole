e8e69cc5c6b3ce22cae3430ce1b4271f3bdf22a2
Raaf88 <rafalewandowski8@gmail.com>
2023-01-23 06:12:18 +0100

    Stworzenie dwóch klas i stworzenie obiektu typu Party

--- src/main/java/rafal/lewandowski/Main.java
+++ src/main/java/rafal/lewandowski/Main.java
@@ -19,7 +19,7 @@ public class Main {
 
             switch (userChoice) {
                 case 1 -> System.out.println(party.getGuests());
-                case 2 -> party.addGuests("Rafal");
+                case 2 -> party.addGuests();
                 case 3 -> System.out.println("Wyświetlanie potraw");
                 case 4 -> System.out.println("Znajdowanie gościa po numerze telefonu");
                 case 5 -> shouldContinue = false;
--- src/main/java/rafal/lewandowski/Party.java
+++ src/main/java/rafal/lewandowski/Party.java
@@ -2,14 +2,19 @@ package rafal.lewandowski;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Party {
     private List<String> guests = new ArrayList<>();
+    Scanner scanner = new Scanner(System.in);
 
-    public void addGuests(String name) {
+    public void addGuests() {
+        System.out.println("Podaj imię gościa: ");
+        String name = scanner.nextLine();
         guests.add(name);
     }
-    public List<String>getGuests(){
+
+    public List<String> getGuests() {
         return guests;
     }
 }
